6fe49b3db978c53d77b0d60f36b679dafb5d363c
Matt Vanek <matt@rootless.net>
2025-05-15 16:04:29 -0500

    Test db-building workflow.

---
+++ ci-db.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+PKGS=(sdk openlibm libcxx fast_float
+      bzip2 zlib xz zstd libarchive libdeflate
+      mbedtls wolfssl
+     )
+
+sudo pacman --noconfirm --remove ps5-payload-dev
+
+for PKG in ${PKGS[*]} ; do
+    pushd $PKG || exit 1
+    rm -f *.pkg.tar.gz
+    rm -rf src pkg
+    makepkg -cisf --noconfirm || exit 1
+    mv ps5-payload-*.pkg.tar.gz ../repo || exit 1
+    popd
+done
+repo-add repo/ps5-payload-dev.db.tar.gz repo/*.pkg.tar.gz
