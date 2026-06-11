7cf3701967d7c207cda13024b9db1161ca11e395
Matthew Vanek <matt@rootless.net>
2026-06-10 22:20:01 -0500

    ci: Restore to upstream version.

--- ci-libs.sh
+++ ci-libs.sh
@@ -31,7 +31,7 @@ for PKG in ${PKGS[*]} ; do
     pushd $PKG || exit 1
     rm -f *.pkg.tar.gz
     rm -rf src pkg
-    makepkg --noconfirm -Ccisr --config ../makepkg.conf -f || exit 1
-    sudo pacman --noconfirm -U ./ps5-payload-*.pkg.tar.* || exit 1
+    makepkg -c -f -C || exit 1
+    sudo pacman --noconfirm -U ./ps5-payload-*.pkg.tar.gz || exit 1
     popd
 done
